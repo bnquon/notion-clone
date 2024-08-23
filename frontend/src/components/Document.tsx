cfe5a3ad0dfbe1fec7cba2929b6f6a97c444225a
Brandon Quon <bqa7@sfu.ca>
2024-08-23 12:32:14 -0700

    added autofocus to the title of a new document

--- frontend/src/components/Document.tsx
+++ frontend/src/components/Document.tsx
@@ -9,6 +9,7 @@ type DocumentProps = {
 
 export const Document = ({ closeDocument }: DocumentProps) => {
   const ref = useRef<EditorJS>();
+  const titleRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     if (!ref.current) {
@@ -26,12 +27,19 @@ export const Document = ({ closeDocument }: DocumentProps) => {
     };
   }, []);
 
+  useEffect(() => {
+    setTimeout(() => {
+      titleRef.current?.focus();
+    }, 0);
+  }, []);
+  
+
   return (
     <div className="w-full h-full flex flex-col">
       <div className="w-full h-fit mb-[10vh] py-8 px-8 flex font-medium">
         {/* Make input tag and put ref here and then autofocus it to be the title 
         + make a pass through state to check if its a new document or not */}
-        <input type="text" name="title" id="title" className="text-2xl outline-none font-medium" placeholder="Document Title" />
+        <input ref={titleRef} type="text" name="title" id="title" className="text-2xl outline-none font-medium" placeholder="Document Title" />
         <button
           onClick={() => closeDocument("home")}
           className="ml-auto text-2xl py-2 px-2 rounded-xl bg-[#f3f5f6] text-black font-medium drop-shadow-lg"
