9d2e2e00eef9d895ea1be77143c071a8ab0e345d
Brandon Quon <bqa7@sfu.ca>
2024-08-24 18:10:07 -0700

    Implemented barbone save features with proper checks to update the user if theyve saved

--- frontend/src/components/Document.tsx
+++ frontend/src/components/Document.tsx
@@ -11,6 +11,8 @@ type DocumentProps = {
 // Make function for the save popup to be a check if its saved then if yes then close document if not then popup
 
 export const Document = ({ closeDocument }: DocumentProps) => {
+  const [isSaved, setIsSaved] = useState<Boolean>(false);
+  const [showSavePopup, setShowSavePopup] = useState<Boolean>(false);
   const ref = useRef<EditorJS>();
   const titleRef = useRef<HTMLInputElement>(null);
 
@@ -21,6 +23,9 @@ export const Document = ({ closeDocument }: DocumentProps) => {
         defaultBlock: "header",
         holder: "editorjs",
         tools: EDITOR_JS_TOOLS,
+        onChange: () => {
+          setIsSaved(false);
+        },
       });
     }
     return () => {
@@ -35,25 +40,57 @@ export const Document = ({ closeDocument }: DocumentProps) => {
       titleRef.current?.focus();
     }, 0);
   }, []);
-  
+
+  const checkIfSavedOrPopup = () => {
+    if (isSaved === true) {
+      closeDocument("home");
+    } else {
+      setShowSavePopup(true);
+    }
+  };
+
+  // Make function to save the document
+  const saveDocumentOrExit = (operation: String) => {
+    if (operation === "save") {
+      // Call eventual async save function
+      setShowSavePopup(false);
+      setIsSaved(true);
+      closeDocument("home");
+    } 
+    if (operation === "exit") {
+      // Do not save!
+      setShowSavePopup(false);
+      closeDocument("home");
+    }
+  }
 
   return (
     <div className="w-full h-full flex flex-col relative">
       <div className="w-full h-fit mb-[10vh] py-8 px-8 flex font-medium">
-        {/* Make input tag and put ref here and then autofocus it to be the title 
-        + make a pass through state to check if its a new document or not */}
-        <input ref={titleRef} type="text" name="title" id="title" className="text-2xl outline-none font-medium" placeholder="Document Title" />
-        <span className="text-xl ml-auto mr-8 shadow-lg rounded-lg px-2 py-2 cursor-pointer outline outline-[#8A2BE2] hover:bg-[#8A2BE2]/90 bg-[#8A2BE2] text-white">
+        <input
+          ref={titleRef}
+          type="text"
+          name="title"
+          id="title"
+          className="text-2xl outline-none font-medium"
+          placeholder="Document Title"
+        />
+        <span
+          onClick={() => setIsSaved(true)}
+          className="text-xl ml-auto mr-8 shadow-lg rounded-lg px-2 py-2 cursor-pointer outline outline-1 outline-[#8A2BE2] hover:bg-[#8A2BE2]/90 bg-[#8A2BE2] text-white"
+        >
           Save Changes
         </span>
         <span
-          onClick={() => closeDocument("home")}
-          className="text-xl py-2 px-2 rounded-lg bg-[#f3f5f6] hover:bg-[#e6e6e6] text-black outline outline-[#8A2BE2] font-medium cursor-pointer drop-shadow-lg"
+          onClick={checkIfSavedOrPopup}
+          className="text-xl py-2 px-2 rounded-lg bg-[#f3f5f6] hover:bg-[#e6e6e6] text-black outline outline-1 outline-[#8A2BE2] font-medium cursor-pointer drop-shadow-lg"
         >
           Close Document
         </span>
       </div>
       <div id="editorjs" className="w-full h-full"></div>
+
+      {showSavePopup && <SavePopup saveDocumentOrExit={saveDocumentOrExit}/>}
     </div>
   );
 };
--- frontend/src/components/SavePopup.tsx
+++ frontend/src/components/SavePopup.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React from "react";
 
-export const SavePopup = () => {
+type SavePopupProps = {
+  saveDocumentOrExit: (operation: String) => void;
+};
+
+export const SavePopup = ({ saveDocumentOrExit }: SavePopupProps) => {
   return (
-    <div>SavePopup</div>
-  )
-}
+    <div className="w-full h-full bg-black/30 flex justify-center items-center absolute">
+      <div className="flex justify-center items-center bg-[#f3f5f6] rounded-lg drop-shadow-lg px-10 py-8 flex-col gap-4">
+        <p className="text-2xl">
+          Page was not saved. Do you want to keep the changes?
+        </p>
+        <div className="flex w-full justify-start">
+          <span
+            onClick={() => saveDocumentOrExit("save")}
+            className="px-3 py-1 text-xl rounded-lg bg-[#8A2BE2] drop-shadow-lg text-white mr-8 cursor-pointer"
+          >
+            Save
+          </span>
+          <span
+            onClick={() => saveDocumentOrExit("exit")}
+            className="px-3 py-1 text-xl rounded-lg drop-shadow-md outline outline-1 outline-[#9e9e9e] bg-white cursor-pointer"
+          >
+            Exit without saving
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
