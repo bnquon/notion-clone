bf34e3fa4fbf5830bae7dd969c23128545bcc8f9
Brandon Quon <bqa7@sfu.ca>
2024-08-25 13:04:19 -0700

    Created getters and setters for @Entity Document

--- backend/src/main/java/com/brandon/backend/model/Document.java
+++ backend/src/main/java/com/brandon/backend/model/Document.java
@@ -10,4 +10,27 @@ public class Document {
     private String content;
 
     // Getters and setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
 }
