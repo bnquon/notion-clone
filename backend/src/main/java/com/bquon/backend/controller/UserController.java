4d57c25fb4f98ebe842b68523468f3fdb3298741
Brandon Quon <bqa7@sfu.ca>
2024-09-01 14:57:20 -0700

    Working on create user api endpoints

--- backend/src/main/java/com/bquon/backend/controller/UserController.java
+++ backend/src/main/java/com/bquon/backend/controller/UserController.java
@@ -3,11 +3,14 @@ package com.bquon.backend.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bquon.backend.model.User;
 import com.bquon.backend.service.UserService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("/users")
@@ -24,4 +27,14 @@ public class UserController {
         return userService.getAllUsers();
     }
 
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PostMapping("/login")
+    public User loginUser(User user) {
+        // Implement login logic here
+        return user;
+    }
 }
