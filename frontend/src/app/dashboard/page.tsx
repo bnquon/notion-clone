77f07e0fca1a3af257726af35e5574c74f17d6cd
Brandon Quon <bqa7@sfu.ca>
2024-09-02 13:20:37 -0700

    Get request for documents with a specific userID integration works

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,10 +1,28 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { SideNav } from "@/components/SideNav";
 import { Document } from "@/components/Document";
 
 export default function Page() {
   const [currentPage, setCurrentPage] = useState<String>("home");
+
+  useEffect(() => {
+    retrieveDocumentsByUserID.refetch();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  // This does work but have to store userID in session storage
+  const retrieveDocumentsByUserID = useQuery({
+    queryKey: ["retrieveDocumentsByUserID"],
+    queryFn: async () => {
+      const userID = 1;
+      const response = await axios.get(`http://localhost:8080/documents/user/${userID}`);
+      console.log(response.data);
+      return response.data;
+    },
+  })
 
   const showDocument = (page: String) => {
     if (page === "createDocument") {
