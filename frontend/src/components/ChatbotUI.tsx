09490e8f39e40073da3df7fac2b51982a62a9ec0
Brandon Quon <bqa7@sfu.ca>
2024-09-11 18:50:57 -0700

    Implemented messaging ui and functionality, needs tweaking however

--- frontend/src/components/ChatbotUI.tsx
+++ frontend/src/components/ChatbotUI.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useMutation } from "@tanstack/react-query";
 import axios from 'axios';
 
@@ -6,19 +6,23 @@ export const ChatbotUI = () => {
 
   // Have to send message and make that into a block into the section and then retrieve llama response and display it in the section
   const userMessageRef = useRef<HTMLTextAreaElement>(null);
+  const [messages, setMessages] = useState<string[]>([]);
 
   const sendMessageFunction = useMutation({
     mutationFn: async () => {
         const response = await axios.post("http://localhost:8080/llama/chat", {
             input: userMessageRef.current!.value
         });
-        console.log(response);
+        setMessages([...messages, response.data.toString()]);
+    },
+    onSuccess: () => {
+        userMessageRef.current!.value = "";
     }
   })
 
   const handleMessageSubmit = () => {
     if (userMessageRef.current?.value !== "") {
-      console.log("userMessageRef.current?.value: ", userMessageRef.current?.value);
+      setMessages([...messages, userMessageRef.current!.value]);
       sendMessageFunction.mutate();
     } else {
         // Show toast later
@@ -28,8 +32,17 @@ export const ChatbotUI = () => {
 
   return (
     <div className='fixed bottom-[3%] right-[3%] w-1/5 h-1/2 flex flex-col border-2 border-black rounded-xl overflow-hidden'>
-      <div className='h-[85%] bg-white grid grid-flow-col'>
+      <div className='h-[85%] bg-white grid grid-flow-row overflow-y-scroll py-4 gap-4'>
         {/* Chat content area */}
+        {messages.map((message: string, index: number) => (
+          <div key={index} className='w-full h-fit text-white flex px-4'
+          style={{ justifyContent: index % 2 === 0 ? "flex-end" : "flex-start"}}>
+            <div className='max-w-[65%] w-fit h-fit p-2 text-xl rounded-xl'
+            style={{ backgroundColor: index % 2 === 0 ? "blue" : "black" }}>
+              <p>{message}</p>
+            </div>
+          </div>
+        ))}
       </div>
       <div className='h-[15%] flex border-t-2 border-black'>
         <textarea ref={userMessageRef}
