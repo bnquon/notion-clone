8533afd4b73c1e3bc6f8f59f35e0b5fc63a65a01
Brandon Quon <bqa7@sfu.ca>
2024-09-12 19:26:47 -0700

    Created signup form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
 export default function Home() {
   const router = useRouter();
+
+  const [isLogin, setIsLogin] = useState<boolean>(true);
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
-  const loginUser =  useMutation({
+  const loginUser = useMutation({
     mutationFn: async () => {
       // console.log(usernameRef.current!.value, passwordRef.current!.value);
       const response = await axios.post('http://localhost:8080/users/login', {
@@ -26,10 +28,29 @@ export default function Home() {
     }
   })
 
+  const createUser = useMutation({
+    mutationFn: async () => {
+      const response = await axios.post('http://localhost:8080/users/create', {
+        username: usernameRef.current!.value,
+        password: passwordRef.current!.value
+      })
+      sessionStorage.setItem("username", response.data.username);
+      sessionStorage.setItem("userID", response.data.userID); 
+      return response.data;
+    },
+    onSuccess: () => {
+      router.push("/dashboard");
+    }
+  })
+
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     if (usernameRef.current?.value !== "" && passwordRef.current?.value !== "") {
-      loginUser.mutate();
+      if (isLogin) {
+        loginUser.mutate();
+      } else {
+        createUser.mutate();
+      }
     } else {
       // Change to a toast later
       alert("Please enter both username and password");
@@ -65,9 +86,12 @@ export default function Home() {
             type="submit"
             className="bg-white w-fit px-8 py-2 rounded-xl ml-auto mr-auto drop-shadow-lg text-xl hover:bg-[#8A2BE2] duration-150 hover:text-white"
           >
-            Login
+            {isLogin ? "Login": "Sign up"}
           </button>
         </form>
+        <p className="mt-10 underline font-bold text-lg cursor-pointer hover:text-[#8A2BE2]" onClick={() => setIsLogin(!isLogin)}>
+          {isLogin ? "Sign up instead" : "Login instead"}
+        </p>
       </div>
     </div>
   );
